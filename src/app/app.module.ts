674b3e3cd5bf20be319b1ab836fbe5b98d308072
Alessandro <alessandro.delloste1@gmail.com>
2017-06-04 19:39:30 +0200

    Creazione header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    SharedModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent
+  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
