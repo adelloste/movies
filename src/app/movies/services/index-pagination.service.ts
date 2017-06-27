ba961f37801fac218e579ab007682a3557f45b13
Alessandro <alessandro.delloste1@gmail.com>
2017-06-27 23:05:48 +0200

    Update card component

--- src/app/movies/services/index-pagination.service.ts
+++ src/app/movies/services/index-pagination.service.ts
@@ -2,15 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class IndexPaginationService {
-
-  // private _bar:boolean = false;
-  
-  // get bar():boolean {
-  //     return this._bar;
-  // }
-  // set bar(theBar:boolean) {
-  //     this._bar = theBar;
-  // }
   
   private _index: number = 1;
 
